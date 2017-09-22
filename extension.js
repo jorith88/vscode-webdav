7ce9950d3cfb7d792dd3063ed1cf6fdefcd9f864
Jorith van den Heuvel <jorith@isaac.nl>
2017-09-22 08:45:36 +0200

    move webdav credentials to a separate file

--- extension.js
+++ extension.js
@@ -6,19 +6,21 @@ const path = require('path');
 const statusBar = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 5);
 
     function activate(context) {
-        var uploadCommand = vscode.commands.registerCommand('extension.webdavUpload', function(){
+        const uploadCommand = vscode.commands.registerCommand('extension.webdavUpload', function(){
             const editor = vscode.window.activeTextEditor;
 
             const workingFile = editor.document.uri.fsPath;
             const workingDir = workingFile.slice(0, workingFile.lastIndexOf(path.sep));
             const configFile = findConfig('webdav.json', {cwd: workingDir});
+            const credConfigFile = findConfig('webdav-credentials.json', {cwd: workingDir});
 
-            if (configFile != null) {
+            if (configFile != null && credConfigFile != null) {
                 // The location of the config file is the directory we use as webdav root
                 const baseDir = configFile.slice(0, configFile.lastIndexOf(path.sep));
 
                 // Read configuration
-                var config = JSON.parse(fs.readFileSync(configFile));
+                const config = JSON.parse(fs.readFileSync(configFile));
+                const credentials = JSON.parse(fs.readFileSync(credConfigFile));
 
                 // Ignore SSL errors, needed for self signed certificates
                 if (config.ignoreSSLErrors) {
@@ -26,16 +28,16 @@ const statusBar = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Le
                 }
 
                 // Initialize WebDAV
-                const webdav =   require("webdav-fs")(config.url, config.user, config.password);
+                const webdav =   require("webdav-fs")(config.url, credentials.user, credentials.password);
                 const remoteFile = workingFile.replace(baseDir, '').replace(/\\/g, '/'); // On Windows replace \ with /
 
                 // Upload
                 webdav.writeFile(remoteFile, editor.document.getText(), function(err) {
                     if (err != null) {
                         console.error(err);
-                        vscode.window.showInformationMessage('Failed to upload file to remote host: ' + err.message);
+                        vscode.window.showErrorMessage('Failed to upload file to remote host: ' + err.message);
                     } else {
-                        var fileName = remoteFile.slice(remoteFile.lastIndexOf('/') + 1);
+                        const fileName = remoteFile.slice(remoteFile.lastIndexOf('/') + 1);
 
                         statusBar.text = "$(cloud-upload) Uploaded " + fileName + "...";
                         statusBar.command = null;
@@ -46,6 +48,8 @@ const statusBar = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Le
                         }, 2000)
                     }
                 });
+            } else {
+                vscode.window.showErrorMessage('Configuration files for WebDAV (webdav.json/webdav-credentials.json) not found...');
             }
         });
 
