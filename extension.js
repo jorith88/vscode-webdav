3c0d21f788632441fcfeeec0422ef3c75dfe18c5
Jorith van den Heuvel <jorith@isaac.nl>
2017-09-27 15:04:01 +0200

    changed setup of remote endpoint configuration

--- extension.js
+++ extension.js
@@ -7,8 +7,8 @@ const tmp = require('tmp');
 const statusBar = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 5);
 
 function activate(context) {
-    const uploadCommand = vscode.commands.registerCommand('extension.webdavUpload', webdavUpload);
-    const compareCommand = vscode.commands.registerCommand('extension.webdavCompare', webdavCompare);
+    const uploadCommand = vscode.commands.registerCommand('extension.webdavUpload', upload);
+    const compareCommand = vscode.commands.registerCommand('extension.webdavCompare', compare);
 
     statusBar.command = 'extension.webdavUpload';
     statusBar.text = '$(cloud-upload) Upload to WebDAV';
@@ -28,107 +28,129 @@ function deactivate() {
 
 exports.deactivate = deactivate;
 
-function webdavUpload() {
-    const editor = vscode.window.activeTextEditor;
+function upload() {
+    doWebdavAction(function(webdav, workingFile, remoteFile) {
+        const editor = vscode.window.activeTextEditor;
 
-    const workingFile = editor.document.uri.fsPath;
-    const workingDir = workingFile.slice(0, workingFile.lastIndexOf(path.sep));
-    const configFile = findConfig('webdav.json', {cwd: workingDir});
-    const credConfigFile = findConfig('webdav-credentials.json', {cwd: workingDir});
-
-    if (configFile != null && credConfigFile != null) {
-        // The location of the config file is the directory we use as webdav root
-        const baseDir = configFile.slice(0, configFile.lastIndexOf(path.sep));
-
-        // Read configuration
-        const config = JSON.parse(fs.readFileSync(configFile));
-        const credentials = JSON.parse(fs.readFileSync(credConfigFile));
-
-        // Ignore SSL errors, needed for self signed certificates
-        if (config.ignoreSSLErrors) {
-            process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
-        }
-
-        // Initialize WebDAV
-        const webdav =   require("webdav-fs")(config.url, credentials.user, credentials.password);
-        const remoteFile = workingFile.replace(baseDir, '').replace(/\\/g, '/'); // On Windows replace \ with /
-
-        // Upload
-        webdav.writeFile(remoteFile, editor.document.getText(), function(err) {
+        webdav.writeFile(remoteFile, editor.document.getText() , function(err) {
             if (err != null) {
                 console.error(err);
                 vscode.window.showErrorMessage('Failed to upload file to remote host: ' + err.message);
             } else {
                 const fileName = remoteFile.slice(remoteFile.lastIndexOf('/') + 1);
 
-                statusBar.text = "$(cloud-upload) Uploaded " + fileName + "...";
+                statusBar.text    = "$(cloud-upload) Uploaded " + fileName + "...";
                 statusBar.command = null;
+                statusBar.color   = '#4cff4c';
 
                 setTimeout(function() {
-                    statusBar.text = '$(cloud-upload) Upload to WebDAV';
+                    statusBar.text    = '$(cloud-upload) Upload to WebDAV';
                     statusBar.command = 'extension.webdavUpload';
+                    statusBar.color   = '#fff';
                 }, 2000)
             }
         });
-    } else {
-        vscode.window.showErrorMessage('Configuration files for WebDAV (webdav.json/webdav-credentials.json) not found...');
-    }
+    });
 }
 
-function webdavCompare() {
-        const editor = vscode.window.activeTextEditor;
+function compare() {
+    doWebdavAction(function(webdav, workingFile, remoteFile) {
 
-        const workingFile = editor.document.uri.fsPath;
-        const workingDir = workingFile.slice(0, workingFile.lastIndexOf(path.sep));
-        const configFile = findConfig('webdav.json', {cwd: workingDir});
-        const credConfigFile = findConfig('webdav-credentials.json', {cwd: workingDir});
+        // Write the remote file to a local temporary file
+        const extension = workingFile.slice(workingFile.lastIndexOf('.'));
+        var tmpFile = tmp.fileSync({ postfix: extension });
+        webdav.readFile(remoteFile, "utf8", function(error, data) {
+            fs.writeFileSync(tmpFile.name, data, function(err) {
+                if(err) {
+                    return console.log(err);
+                }
+            });
 
-        if (configFile != null && credConfigFile != null) {
-            // The location of the config file is the directory we use as webdav root
-            const baseDir = configFile.slice(0, configFile.lastIndexOf(path.sep));
+            // Compare!
+            try {
+                const fileName = remoteFile.slice(remoteFile.lastIndexOf('/') + 1);
 
-            // Read configuration
-            const config = JSON.parse(fs.readFileSync(configFile));
-            const credentials = JSON.parse(fs.readFileSync(credConfigFile));
+                vscode.commands.executeCommand('vscode.diff',
+                    vscode.Uri.file(tmpFile.name),
+                    vscode.Uri.file(workingFile),
+                    fileName + ' (WebDAV Compare)',
+                    {
+                        preview: false, // Open the diff in an additional tab instead of replacing the current one
+                        selection: null // Don't select any text in the compare
+                    });
+
+            } catch (error) {
+                console.log(error);
+            }
+        });
+    });
+}
+
+function doWebdavAction(webdavAction) {
+    const workingFile = vscode.window.activeTextEditor.document.uri.fsPath;
+    const workingDir = workingFile.slice(0, workingFile.lastIndexOf(path.sep));
+    const credConfigFile = findConfig('webdav-credentials.json', {cwd: workingDir});
+
+    if (credConfigFile != null) {
+
+        // Read configuration
+        const config = getEndpointConfigForCurrentPath(workingDir);
+        const credentials = JSON.parse(fs.readFileSync(credConfigFile));
+
+        if (config != null) {
 
             // Ignore SSL errors, needed for self signed certificates
-            if (config.ignoreSSLErrors) {
+            if (config.remoteEndpoint.ignoreSSLErrors) {
                 process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
             }
 
             // Initialize WebDAV
-            const webdav =   require("webdav-fs")(config.url, credentials.user, credentials.password);
-            const remoteFile = workingFile.replace(baseDir, '').replace(/\\/g, '/'); // On Windows replace \ with /
+            const webdav =   require("webdav-fs")(config.remoteEndpoint.url, credentials.user, credentials.password);
+            const remoteFile = workingFile.replace(/\\/g, '/').replace(vscode.workspace.rootPath.replace(/\\/g, '/') + config.localRootPath, ''); // On Windows replace \ with /
 
-            // Write the remote file to a local temporary file
-            const extension = workingFile.slice(workingFile.lastIndexOf('.'));
-            var tmpFile = tmp.fileSync({ postfix: extension });
-            webdav.readFile(remoteFile, "utf8", function(error, data) {
-                fs.writeFileSync(tmpFile.name, data, function(err) {
-                    if(err) {
-                        return console.log(err);
-                    }
-                });
-
-                // Compare!
-                try {
-                    const fileName = remoteFile.slice(remoteFile.lastIndexOf('/') + 1);
-
-                    vscode.commands.executeCommand('vscode.diff',
-                        vscode.Uri.file(tmpFile.name),
-                        vscode.Uri.file(workingFile),
-                        fileName + ' (WebDAV Compare)',
-                        {
-                            preview: false, // Open the diff in an additional tab instead of replacing the current one
-                            selection: null // Don't select any text in the compare
-                        });
-
-                } catch (error) {
-                    console.log(error);
-                }
-            });
-
-        } else {
-            vscode.window.showErrorMessage('Configuration files for WebDAV (webdav.json/webdav-credentials.json) not found...');
+            webdavAction(webdav, workingFile, remoteFile);
         }
+
+    } else {
+        vscode.window.showErrorMessage('Credentials config file for WebDAV (webdav-credentials.json) not found...');
+    }
+}
+
+function getEndpointConfigForCurrentPath(absoluteWorkingDir) {
+    const configFile = findConfig('webdav.json', {cwd: absoluteWorkingDir});
+
+    if (configFile != null) {
+        var allEndpointsConfig = JSON.parse(fs.readFileSync(configFile));
+
+        var endpointConfigDirectory = configFile.slice(0, configFile.lastIndexOf(path.sep));
+
+        const relativeWorkingDir = absoluteWorkingDir.slice(endpointConfigDirectory.length).replace(/\\/g, '/'); // On Windows replace \ with /
+
+        let endpointConfig = null;
+        let currentSearchPath = relativeWorkingDir;
+        let configOnCurrentSearchPath = null;
+
+        while (!endpointConfig) {
+
+            if (currentSearchPath === "") {
+                vscode.window.showErrorMessage('Cannot find a remote endpoint configuration for the current working directory ' + relativeWorkingDir + ' in webdav.json...');
+                return null;
+            }
+
+            configOnCurrentSearchPath = allEndpointsConfig[currentSearchPath];
+
+            if (configOnCurrentSearchPath) {
+                endpointConfig = configOnCurrentSearchPath;
+            } else {
+                currentSearchPath = currentSearchPath.slice(0, currentSearchPath.lastIndexOf("/"));
+            }
+        }
+
+        return {
+            localRootPath: currentSearchPath,
+            remoteEndpoint: endpointConfig
+        }
+    } else {
+        vscode.window.showErrorMessage('Endpoint config file for WebDAV (webdav.json) not found...');
+    }
 }
